174dbe8d16f266cb207e29722e335b67ca81002f
Rabunk <vanhnotvanh@gmail.com>
2025-12-25 23:39:23 +0700

    Delete backend directory

--- backend/routers/auth.router.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { handleLogin, handleResgiter } from '../controllers/auth.controller.js';
-
-const router = express.Router();
-
-router.post('/login', handleLogin)
-router.post('/register', handleResgiter)
-
-export default router
--- backend/routers/dashboard.router.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getDashboardSummary } from "../controllers/dashboard.controller.js";
-import { authentication, isAdmin } from "../middlewares/auth.middleware.js";
-
-const router = express.Router();
-
-router.get('/summary', getDashboardSummary);
-
-export default router;
